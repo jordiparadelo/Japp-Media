b097f0a9b6654e765b92403f80c8665e9602e9ce
Jordi <im.jordiparadelo@gmail.com>
2024-09-22 13:11:45 +0200

    Revert "Deployment fixes"
    This reverts commit 42d6610128e32ba160e1683bde35722a11dd07ea.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,32 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	distDir: 'dist',
-	experimental: {
-		appDir: true,
+	output: 'export',  // Add this line
+	webpack(config) {
+		// Grab the existing rule that handles SVG imports
+		const fileLoaderRule = config.module.rules.find((rule) =>
+			rule.test?.test?.(".svg")
+		);
+
+		config.module.rules.push(
+			// Reapply the existing rule, but only for svg imports ending in ?url
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/, // *.svg?url
+			},
+			// Convert all other *.svg imports to React components
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+				use: ["@svgr/webpack"],
+			}
+		);
+
+		// Modify the file loader rule to ignore *.svg, since we have it handled now.
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		return config;
 	},
-	// Add this line to specify the source directory
-	dir: './src',
 };
 
 export default nextConfig;
